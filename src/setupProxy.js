1c74d937b4f743e74e387fc540a0e966a2714050
ZenithGD <darimarcos456@hotmail.com>
2022-05-13 12:07:14 +0200

    setup proxy only in development

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,11 +3,13 @@ const { createProxyMiddleware } = require("http-proxy-middleware")
 const API_URL = process.env.REACT_APP_API_ENDPOINT;
 
 module.exports = function (app) {
-    app.use(
-        "/api",
-        createProxyMiddleware({
-            target: API_URL,
-            changeOrigin: true,
-        })
-    )
+    if ( process.env.NODE_ENV == "development" ) {
+        app.use(
+            "/api",
+            createProxyMiddleware({
+                target: API_URL,
+                changeOrigin: true,
+            })
+        )
+    }
 }
