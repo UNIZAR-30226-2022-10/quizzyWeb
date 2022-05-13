1af241df2ef7de51a8743f4283e8dfce44996bbd
ZenithGD <darimarcos456@hotmail.com>
2022-05-13 02:29:52 +0200

    update proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,12 @@
 const { createProxyMiddleware } = require("http-proxy-middleware")
 
+const API_URL = process.env.REACT_APP_API_ENDPOINT;
+
 module.exports = function (app) {
     app.use(
         "/api",
         createProxyMiddleware({
-            target: "http://localhost:5000",
+            target: API_URL,
             changeOrigin: true,
         })
     )
