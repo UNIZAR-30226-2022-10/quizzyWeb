d550fd849fe8fda6649adf60d14ae7d63839194f
ZenithGD <darimarcos456@hotmail.com>
2022-05-15 01:43:17 +0200

    setup proxy ok

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,8 @@ const { createProxyMiddleware } = require("http-proxy-middleware")
 const API_URL = process.env.REACT_APP_API_ENDPOINT;
 
 module.exports = function (app) {
-    if ( process.env.NODE_ENV == "development" ) {
+    if ( process.env.ENABLE_PROXY === "yes" ) {
+        console.log("Enabled proxy")
         app.use(
             "/api",
             createProxyMiddleware({
