9073a4e813c9f092d584698b43ea348b7eaefe2d
ZenithGD <darimarcos456@hotmail.com>
2022-05-15 22:38:31 +0200

    set montserrat font for every mui component

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,6 +1,13 @@
 import { styled, ThemeProvider, createTheme } from "@mui/material/styles"
 
 const theme = createTheme({
+    typography: {
+        "fontFamily": `"montserrat", "Helvetica", "Arial", sans-serif`,
+        "fontSize": 14,
+        "fontWeightLight": 300,
+        "fontWeightRegular": 400,
+        "fontWeightMedium": 500
+    },
     components: {
         MuiIcon: {
             styleOverrides: {
