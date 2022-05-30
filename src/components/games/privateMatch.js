556f3ee3c3430923de9e17daf63120b69ac0156a
Diana-Saluena <795874@unizar.es>
2022-05-30 21:26:20 +0200

    Co-authored-by: Darío Marcos <ZenithGD@users.noreply.github.com>

---
+++ src/components/games/privateMatch.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function PrivateMatch() {
+    //PrivateMatch contain players, winner, configuration
+  return (
+    <div>privateMatch</div>
+  )
+}
+
+export default PrivateMatch
--- src/views/stats.js
+++ src/views/stats.js
@@ -3,10 +3,44 @@ import Container from "@mui/material/Container"
 import Box from "@mui/material/Box"
 import {Tabs, Tab} from "@mui/material"
 import { useTheme } from "@mui/material"
+import { Grid } from '@mui/material'
+import { Card } from '@mui/material'
+import { Typography } from '@mui/material'
+import { CardContent } from '@mui/material'
+
+import { capitalizeFirstLetter } from "utils/stringService"
+
+import PublicMatch from "../components/games/publicMatch"
 
 import "index.css"
 
+const players = [
+    { nickname : "hola", actual_cosmetic : 3 },
+    { nickname : "hola2", actual_cosmetic : 1 },
+    { nickname : "hola3", actual_cosmetic : 2 },
+    { nickname : "hola4", actual_cosmetic : 5 },
+    { nickname : "hola5", actual_cosmetic : 1 },
+    { nickname : "hola6", actual_cosmetic : 7 },
+]
+
 export default function Stats() {
+
+    function TabPanel(props) {
+        const { children, value, index, ...other } = props
+
+        return (
+            <div
+                role="tabpanel"
+                hidden={value !== index}
+                id={`simple-tabpanel-${index}`}
+                aria-labelledby={`simple-tab-${index}`}
+                {...other}
+            >
+                {value === index && <Box sx={{ p: 3 }}> {children} </Box>}
+            </div>
+        )
+    }
+
     const theme = useTheme()
 
     // tab value
@@ -17,12 +51,8 @@ export default function Stats() {
         setValue(v)
     }
 
-    const [partidas] = useState([
-        "Last Game",
-        "Game x",
-        "Game y",
-        "Game z",
-        "Game w",
+    const [matches] = useState([
+        players, players, players
     ]); 
 
     return (
@@ -42,6 +72,19 @@ export default function Stats() {
                     <Tab label={"Historial"}></Tab>
                 </Tabs>
             </Box>
+
+            <TabPanel value={value} index={0}>
+                <div
+                    style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        gap: "1rem"
+                    }}>
+                    {matches.map((item, idx) =>
+                        <PublicMatch key={idx} rid={idx} players={item} winner={item[0].nickname} />
+                    )}
+                </div>
+            </TabPanel>
         </Container>
     )
 }
